8b9a48cc05212dbe22b733d8b5508cfa1f85c507
Rattenfengar <37422018+Rattenfengar@users.noreply.github.com>
2019-03-20 22:39:13 -0500

    New Description added to Car Model

--- ParkinApp/ParkinApp/Model/Car.cs
+++ ParkinApp/ParkinApp/Model/Car.cs
@@ -5,9 +5,18 @@ using System.Text;
 
 namespace ParkinApp.Model
 {
+    /// <summary>
+    /// This class is the model that represents a car which will use the parking lot.
+    /// </summary>
     public class Car
     {
+        /// <summary>
+        /// This property represents the Car ID
+        /// </summary>
         public string LicensePlate { get; set; }
+        /// <summary>
+        /// Time 
+        /// </summary>
         public DateTime InputTime { get; set; }
         public DateTime OutputTime { get; set; }
     }
